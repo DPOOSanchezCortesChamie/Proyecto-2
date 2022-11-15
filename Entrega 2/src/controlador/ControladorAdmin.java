8d59a3a58d944cc5c11eb90624cf5bc46eeb333b
SantiagoChamie <93161971+SantiagoChamie@users.noreply.github.com>
2022-11-14 20:38:30 -0500

    controladores

--- Entrega 2/src/controlador/ControladorAdmin.java
+++ Entrega 2/src/controlador/ControladorAdmin.java
@@ -1,10 +1,25 @@
 package controlador;
 
+import java.util.List;
+
 import modelo.Admin;
+import modelo.EquipoFantasia;
 
 public class ControladorAdmin {
 	Admin admin;
 	public ControladorAdmin(Admin a) {
 		this.admin = a;
+	}
+	public void crearTemporada(String nominas, String fechas, int presupuesto) {
+		admin.crearTemporada(nominas, fechas, presupuesto);
+	}
+	public List<EquipoFantasia> getMejoresTres() {
+		return admin.getMejoresTres();
+	}
+	public void registrarResultadoPartido(String resultado) {
+		admin.registrarResultadoPartido(resultado);
+	}
+	public void concluirFecha() {
+		admin.concluirFecha();
 	}
 }
--- Entrega 2/src/controlador/ControladorUsuario.java
+++ Entrega 2/src/controlador/ControladorUsuario.java
@@ -1,10 +1,24 @@
 package controlador;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import modelo.Jugador;
 import modelo.Participante;
+import modelo.TemporadaFantasia;
 
 public class ControladorUsuario {
 	private Participante usuario;
 	public ControladorUsuario(Participante p) {
 		this.usuario = p;
+	}
+	public HashMap<String, Jugador> getJugadores(String equipo) {
+		return this.usuario.getEquipo(equipo).getJugadores();
+	}
+	public void venderComprarJugadores(String equipo, Jugador saliente, Jugador entrante) {
+		this.usuario.venderComprarJugadores(equipo, saliente, entrante);
+	}
+	public void crearEquipo(String nombre, TemporadaFantasia temporada, ArrayList<Jugador> jugadores) {
+		this.usuario.crearEquipo(nombre, temporada, jugadores);
 	}
 }
